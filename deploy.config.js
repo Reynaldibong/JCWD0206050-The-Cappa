aa4e17d75247a76a6b69390029908f7b9a44f8f1
Ryan Defryan <muhammaddefryan@gmail.com>
2023-10-17 21:29:08 +0700

    Create deploy.config.js

---
+++ deploy.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+ apps: [
+  {
+   name: 'JCWD-0206-050',
+   script: './src/index.js',
+   env: {
+    NODE_ENV: 'production',
+    PORT: 8251
+   },
+   time: true
+  }
+ ]
+};
